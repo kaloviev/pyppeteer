9bc01764db30cc155dd0606056febb2ab9cbaebc
kaloviev <kaloviev@bk.ru>
2022-09-17 13:35:41 +0300

    Use chromium browser

--- pyppeteer/chromium_downloader.py
+++ pyppeteer/chromium_downloader.py
@@ -46,7 +46,7 @@ downloadURLs = {
 }
 
 chromiumExecutable = {
-    'linux': DOWNLOADS_FOLDER / REVISION / 'chrome-linux' / 'chrome',
+    'linux': os.getenv('CHROMIUM_EXEC_PATH'),
     'mac': (DOWNLOADS_FOLDER / REVISION / 'chrome-mac' / 'Chromium.app' / 'Contents' / 'MacOS' / 'Chromium'),
     'win32': DOWNLOADS_FOLDER / REVISION / windowsArchive / 'chrome.exe',
     'win64': DOWNLOADS_FOLDER / REVISION / windowsArchive / 'chrome.exe',
@@ -146,5 +146,4 @@ def chromium_executable() -> Path:
 
 def check_chromium() -> bool:
     """Check if chromium is placed at correct path."""
-    # return chromium_executable().exists()
-    return True
+    return chromium_executable().exists()
